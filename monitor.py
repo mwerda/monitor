94038275462735752db3510649eef6f9f63ba04a
mwerda <zmalqpzmalqp@gmail.com>
2017-10-11 04:26:08 +0200

    Iterating

--- monitor.py
+++ monitor.py
@@ -28,7 +28,9 @@ class Environment:
 class Node:
 
     def __init__(self, events, channels):
-        self.talker = Talker(events, channels)
+        self.enter_critical_section = Event()
+        self.leave_critical_section = Event()
+        self.talker = Talker(events, channels, self.enter_critical_section, self.leave_critical_section)
 
 
 
@@ -37,7 +39,7 @@ class Node:
 class Talker(Thread):
     new_id = 0
 
-    def __init__(self, events, channels):
+    def __init__(self, events, channels, enter_critical_section, leave_critical_section):
         self.id = Talker.new_id
         self.request_numbers = []
         self.token = None
@@ -45,6 +47,8 @@ class Talker(Thread):
         self.all_events = events
         self.channel = channels[self.id]
         self.all_channels = channels
+        self.enter_critical_section = enter_critical_section
+        self.leave_critical_section = leave_critical_section
 
         Talker.new_id += 1
         Thread.__init__(self)
@@ -85,19 +89,53 @@ class Talker(Thread):
 
     # params: {'id': int, 'seq': int (sequential number)
     def handle_incoming_request(self, args):
+        self.event.clear()
         self.request_numbers[args['id']]\
             = args['seq'] if args['seq'] > self.request_numbers[args['id']] else self.request_numbers[args['id']]
         if self.token is not None:
-            pass_token({'id': args['id'], 'token': copy.copy(self.token)})
+            self.pass_token({'id': args['id'], 'token': copy.copy(self.token)})
             self.token = None
 
     def send_request(self, args):
         self.request_numbers[self.id] += 1
         for element in self.all_channels:
             element = {'id': self.id, 'seq': self.request_numbers[self.id]}
+        for event in self.all_events:
+            event.set()
+
 
     def receive_token(self, args):
+        self.event.clear()
+        self.token = copy.copy(self.channel['token'])
+        self.channel = {}
+        self.enter_critical_section.set()
+        self.leave_critical_section.wait()
+        self.token[self.id] = self.request_numbers[self.id]
+        self.token.queue.pop()
 
+        i = 0
+        for token_element, request_element in zip(self.token.queue, self.request_numbers):
+            if request_element > token_element:
+                token_element = request_element
+                self.token.queue.append(i)
+                i += 1
+
+        if len(self.token.queue) > 0:
+            token_copy = copy.copy(self.token)
+            args = {'id': token_copy.queue[0], 'token': token_copy}
+            self.token = None
+            self.pass_token(args)
+
+        # skopiuj token do siebie
+        # wyslij sygnal do workera, zeby ogarnal swoja sekcje i zaloz waita na event, czekaj na odblokowanie
+        # wez LN[i] tokenowe = moje RN[i]
+        # queue pop
+        # dla kazdego id, ktorego nie ma w kolejce, dodaj jesli LN jest mniejsze od RN
+        # wywal token do pierwszego w kolejce - skopiuj i usun lokalna kopie
+        return
+
+
+    # params: {'id': int, 'token': token}
     def pass_token(self, args):
         self.all_channels[args['id']] = args
         self.all_events[args['id']].set()
